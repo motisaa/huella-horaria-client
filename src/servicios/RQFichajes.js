2499d9219c17fca77b344b2c3de4732de5d8de93
Moti <motisa94@gmail.com>
2024-04-09 23:24:58 +0200

    feat: add functions to create, edit, read a fichaje using axios

--- src/servicios/RQFichajes.js
+++ src/servicios/RQFichajes.js
@@ -14,3 +14,24 @@ export const EliminarFichaje = (fichajeId) => {
     const url = `${url_base}/v1/fichajes/${fichajeId}`;
     return axios.delete(url);
 };
+
+export const CrearFichaje = (fichajeId) => {
+    const ent = Entorno.getEnv();
+    const url_base = ent.API_URL;
+    const url = `${url_base}/v1/fichajes`;
+    return axios.post(url, fichajeId);
+};
+
+  export const ActualizarFichaje = (fichaje) => {
+    const ent = Entorno.getEnv();
+    const url_base = ent.API_URL;
+    const url = `${url_base}/v1/fichajes`;
+    return axios.put(url, fichaje);
+  };
+  
+  export const LeerFichaje = (fichajeId) => {
+    const ent = Entorno.getEnv();
+    const url_base = ent.API_URL;
+    const url = `${url_base}/v1/fichajes/${fichajeId}`;
+    return axios.get(url);
+  };
