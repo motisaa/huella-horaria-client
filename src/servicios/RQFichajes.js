c1e1def54d79cf1c6de08198c665ab3e41280e96
Moti <motisa94@gmail.com>
2024-04-10 13:40:01 +0200

    feat: axios dunctions to manage fichajes

--- src/servicios/RQFichajes.js
+++ src/servicios/RQFichajes.js
@@ -2,36 +2,36 @@ import axios from "axios";
 import Entorno from "./Entorno";
 
 export const LeerFichajes = () => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/fichajes`;
+  const ent = Entorno.getEnv();
+  const url_base = ent.API_URL;
+  const url = `${url_base}/v1/fichajes`;
 
-    return axios.get(url);
+  return axios.get(url);
 };
 export const EliminarFichaje = (fichajeId) => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/fichajes/${fichajeId}`;
-    return axios.delete(url);
+  const ent = Entorno.getEnv();
+  const url_base = ent.API_URL;
+  const url = `${url_base}/v1/fichajes/${fichajeId}`;
+  return axios.delete(url);
 };
 
 export const CrearFichaje = (fichajeId) => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/fichajes`;
-    return axios.post(url, fichajeId);
+  const ent = Entorno.getEnv();
+  const url_base = ent.API_URL;
+  const url = `${url_base}/v1/fichajes`;
+  return axios.post(url, fichajeId);
 };
 
-  export const ActualizarFichaje = (fichaje) => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/fichajes`;
-    return axios.put(url, fichaje);
-  };
-  
-  export const LeerFichaje = (fichajeId) => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/fichajes/${fichajeId}`;
-    return axios.get(url);
-  };
+export const ActualizarFichaje = (fichaje) => {
+  const ent = Entorno.getEnv();
+  const url_base = ent.API_URL;
+  const url = `${url_base}/v1/fichajes`;
+  return axios.put(url, fichaje);
+};
+
+export const LeerFichaje = (fichajeId) => {
+  const ent = Entorno.getEnv();
+  const url_base = ent.API_URL;
+  const url = `${url_base}/v1/fichajes/${fichajeId}`;
+  return axios.get(url);
+};
