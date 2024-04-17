d913c3523fcfce608a49217758bd7fe2ea1589cb
Moti <motisa94@gmail.com>
2024-04-17 13:30:57 +0200

    feat: prueba leaflet

---
+++ src/componentes/Mapa/Mapa.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export const Mapa = () => {
+    return ( 
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "50px", width: "100%" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='<a href="https://www.openstreetmap.org/copyright"></a>'
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
