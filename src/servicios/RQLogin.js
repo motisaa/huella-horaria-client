b0cbb98eda6eff1b928d97f0d257281dea38387f
Moti <motisa94@gmail.com>
2024-04-19 10:43:43 +0200

    feat: change loginBasicoUsuario ruta

---
+++ src/servicios/RQLogin.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Entorno from "./Entorno";
+
+export const LoginBasicoUsuario = (usuario, password) => {
+    const ent = Entorno.getEnv();
+    const url_base = ent.API_URL;
+    const url = `${url_base}/v1/login`;
+    const data = {
+        usuario,
+        password,
+    };
+    return axios.post(url, data);
+};
--- src/servicios/RQadministradores.js
+++ src/servicios/RQadministradores.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 import Entorno from "./Entorno";
 
-export const LoginBasicoUsuario = (usuario, password) => {
-    const ent = Entorno.getEnv();
-    const url_base = ent.API_URL;
-    const url = `${url_base}/v1/administradores/login`;
-    const data = {
-        usuario,
-        password,
-    };
-    return axios.post(url, data);
-};
-
 export const LeerUsuariosAdmin = () => {
     const ent = Entorno.getEnv();
     const url_base = ent.API_URL;
