3fd81c94ee974bdb1eb200f175e8ab9f57e4c79e
Moti <motisa94@gmail.com>
2024-04-19 20:59:34 +0200

    feat: increase auto hide duration

--- src/componentes/ErrorGeneral/ErrorGeneral.js
+++ src/componentes/ErrorGeneral/ErrorGeneral.js
@@ -6,7 +6,7 @@ export const ErrorGeneral = (props) => {
 
     return (
         <>
-            <Snackbar key={uuid()} open={props.hayError} autoHideDuration={6000} onClose={props.cerrarError}
+            <Snackbar key={uuid()} open={props.hayError} autoHideDuration={10000} onClose={props.cerrarError}
                 anchorOrigin={{ vertical: 'top', horizontal: 'left', }}>
                 <Alert onClose={props.cerrarError} severity="error" sx={{ width: '100%', marginTop: 10 }}>
                     {props.mensajeError}
