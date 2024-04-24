68a96bdd95e94d04e532fe30373412ea8afe8b80
Moti <motisa94@gmail.com>
2024-04-24 10:39:46 +0200

    feat: responsive design for tablet and mobile for trabajadores page

--- src/paginas/TrabajadoresPaginas/TrabajadoresPagina.js
+++ src/paginas/TrabajadoresPaginas/TrabajadoresPagina.js
@@ -6,7 +6,7 @@ import { MensajeError } from "../../servicios/TratamientoErrores";
 import { ErrorGeneral } from "../../componentes/ErrorGeneral/ErrorGeneral";
 import { LeerUsuariosTrabajadores, eliminarUsuarioTrabajador } from "../../servicios/RQTrabajadores";
 import AddIcon from "@mui/icons-material/Add";
-import { AppBar, Grid, IconButton, Toolbar, Tooltip } from "@mui/material";
+import { AppBar, Grid, IconButton, Toolbar, Tooltip, useMediaQuery } from "@mui/material";
 import { MensajeInformativo } from "../../componentes/MensajeInformativo/MensajeInformativo";
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { esES } from '@mui/x-data-grid/locales';
@@ -24,6 +24,9 @@ export const TrabajadoresPagina = () => {
   const [usuarioTrabajador, setUsuarioTrabajador] = useState();
   const [mensajeConfirmacion, setMensajeConfirmacion] = useState("");
   const [hayConfirmacion, setHayConfirmacion] = useState(false);
+  const mobileMediaQuery = useMediaQuery('(max-width: 37.5em)'); // 600px / 16px = 37.5em
+  const tabletMediaQuery = useMediaQuery('(max-width: 64em)'); // 1024px / 16px  = 64em
+
 
   const nuevoUsuarioTrabajador = () => {
     navigate(`/trabajador/0`);
@@ -86,8 +89,50 @@ export const TrabajadoresPagina = () => {
       },
     }
   );
-
-
+  
+  const columnsMobile = [
+    { field: "nombre", headerName: "Nombre", flex: 0.5 },
+    { field: "apellido1", headerName: "Apellido 1", flex: 0.4 },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Acciones",
+      flex: 0.3,
+      getActions: ({ row }) => {
+        return [
+          <IconButton onClick={deleteUsuarioTrabajador(row)}>
+            <DeleteIcon />
+          </IconButton>,
+          <IconButton onClick={editUsuarioTrabjador(row.trabajadorId)}>
+            <EditIcon />
+          </IconButton>,
+        ];
+      },
+    },
+  ]
+   
+  const columnsTablet = [
+    { field: "nombre", headerName: "Nombre", flex: 0.5 },
+    { field: "apellido1", headerName: "Apellido 1", flex: 0.4 },
+    { field: "usuario", headerName: "Nombre de usuario", flex: 0.4 },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Acciones",
+      flex: 0.3,
+      getActions: ({ row }) => {
+        return [
+          <IconButton onClick={deleteUsuarioTrabajador(row)}>
+            <DeleteIcon />
+          </IconButton>,
+          <IconButton onClick={editUsuarioTrabjador(row.trabajadorId)}>
+            <EditIcon />
+          </IconButton>,
+        ];
+      },
+    },
+  ]
+   //for bigger screens laptop or desktop
   const columns = [
     { field: "trabajadorId", headerName: "ID", width: 50 },
     { field: "nombre", headerName: "Nombre", flex: 0.5 },
@@ -146,7 +191,11 @@ export const TrabajadoresPagina = () => {
           <Grid item xs={12} style={{ height: "80vh", width: "100%" }}>
             <DataGrid
               rows={usuariosTrabajador}
-              columns={columns}
+              columns={
+                mobileMediaQuery ? columnsMobile :
+                tabletMediaQuery ? columnsTablet :
+                columns
+              }
               getRowId={(row) => row.trabajadorId}
               localeText={esES.components.MuiDataGrid.defaultProps.localeText}
               slots={{ toolbar: GridToolbar }}
