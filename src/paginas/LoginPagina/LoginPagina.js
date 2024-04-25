021f6fd22fe12c4286f9c41c61d02e87681a5c2d
Moti <motisa94@gmail.com>
2024-04-25 10:00:03 +0200

    feat: add app name for mobile login page

--- src/paginas/LoginPagina/LoginPagina.js
+++ src/paginas/LoginPagina/LoginPagina.js
@@ -1,9 +1,10 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, useMediaQuery } from "@mui/material";
 import React, { useEffect, useState } from 'react';
 import { leerVersion } from '../../servicios/ApiLib';
 import { LoginForm } from './LoginForm';
 
 export const LoginPagina = () => {
+    const mobileMediaQuery = useMediaQuery('(max-width: 37.5em)'); // 600px / 16px = 37.5em
     const [version, setVersion] = useState('0.0.0');
     const consultarVersion = async () => {
         const { data: versionData } = await leerVersion()
@@ -27,6 +28,20 @@ export const LoginPagina = () => {
                         versión: {version}
                     </Typography>
                 </Grid>
+                {mobileMediaQuery
+                    ? ( <Grid item xs={12} sm={6} md={4} 
+                          sx={{marginTop: 5, marginLeft:  12}}>
+                         <Typography variant='h5'>
+                            Huella Horaria
+                         </Typography>
+                         <Typography variant='h6'>
+                            Gestión de fichajes
+                         </Typography>
+                         <Typography>
+                            versión: {version}
+                         </Typography>
+                         </Grid> )
+                    : ''}
                 <Grid item xs={12} sm={6} md={4} className="loginForm">
                     <div className='recuadro'>
                         <LoginForm />
