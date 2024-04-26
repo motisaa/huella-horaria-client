718563e062b32f745fc15b5ad95b0aefdf7db17d
Moti <motisa94@gmail.com>
2024-04-26 13:24:54 +0200

    fixed: date to Spain local time zone

--- src/componentes/MensajeInformativo/MensajeInformativo.js
+++ src/componentes/MensajeInformativo/MensajeInformativo.js
@@ -5,7 +5,7 @@ import { v4 as uuid } from "uuid";
 export const MensajeInformativo = (props) => {
     return (
         <>
-            <Snackbar key={uuid()} open={props.hayMensaje} autoHideDuration={6000} onClose={props.cerrarMensaje}
+            <Snackbar key={uuid()} open={props.hayMensaje} autoHideDuration={9000} onClose={props.cerrarMensaje}
                 anchorOrigin={{ vertical: 'top', horizontal: 'right', }}>
                 <Alert onClose={props.cerrarMensaje} severity="success" sx={{ width: '100%' }}>
                     {props.mensaje}
--- src/servicios/TratamientoFechas.js
+++ src/servicios/TratamientoFechas.js
@@ -5,12 +5,20 @@ import moment from "moment-timezone";
 export const FormatoFechaEs = (fechaMysql) => {
   if (!fechaMysql) return "";
   const dateInUTC = moment.utc(fechaMysql);
-  const dateInLocal = moment(dateInUTC).tz("Europe/Madrid").format();
-  let formatted = moment(dateInLocal).format("DD/MM/YYYY HH:mm:ss");
-
-  return formatted;
+  const dateInLocal = dateInUTC.local('Europe/Madrid');
+  return dateInLocal.format('DD/MM/YYYY HH:mm:ss')
+  // el siguiente codigo funciona bien en localhost pero no en la página web hosteada
+  // const formatted = moment(fechaMysql)
+  //  convertir a la zona horaria de España
+  // .tz("Europe/Madrid")
+  // //formateamos a la fecha española
+  // .format("DD/MM/YYYY HH:mm:ss");
+  // return formatted;
 };
 
 export const ConvertirAFechaEs = (fechaMysql) => {
-  return moment(fechaMysql).tz("Europe/Madrid").format();
+  // return moment(fechaMysql).tz("Europe/Madrid").format();
+  const dateInUTC = moment.utc(fechaMysql);
+  const dateInLocal = dateInUTC.local('Europe/Madrid');
+  return dateInLocal.format()
 };
