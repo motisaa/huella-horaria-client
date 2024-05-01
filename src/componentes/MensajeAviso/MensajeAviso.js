5b7ef8960c6de53a591d68a116f0ceb502eb6aef
Moti <motisa94@gmail.com>
2024-05-01 20:02:19 +0200

    feat: add warn message modal

--- src/componentes/MensajeAviso/MensajeAviso.js
+++ src/componentes/MensajeAviso/MensajeAviso.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
-import { AlertTitle, useMediaQuery } from '@mui/material';
+import { AlertTitle, FormControlLabel, useMediaQuery } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import Checkbox from '@mui/material/Checkbox';
+import { GeneralCtx } from '../../contextos/GeneralContext'; // Import the GeneralContext
 
 export const MensajeAviso = () => {
     const [mostrarMensaje, setMostrarMensaje] = useState(true);
@@ -25,3 +29,57 @@ export const MensajeAviso = () => {
         </Stack>
     );
 }
+
+
+export const WarnMsg = (props) => {
+    const navigate = useNavigate();
+    const [noMostrarMas, setNoMostrarMas] = useState(false)
+    const { setNoMostrarCookie } = useContext(GeneralCtx); // Get the functions from the context
+
+     // Handler for the "Ok" button click
+     const handleOkClick = () => {
+        if (noMostrarMas) {
+            setNoMostrarCookie(true);
+            setNoMostrarMas(true)
+        }
+         props.cerrarAviso(); // Close the modal
+         navigate(`/fichaje/0`);
+    };
+
+    // Handler for the "No mostrar más" checkbox change
+    const handleCheckboxChange = () => {
+        setNoMostrarMas(true); // Update the state of "No mostrar más" checkbox
+        
+    };
+
+    return (
+        <Dialog
+            open={props.aviso}
+            onClose={props.cerrarAviso}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {"De el permiso al navegador cuando vaya al fichar "}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Para obtener las coordenadas geográficas y ver la ubicación
+                    actual en el mapa, es necesario habilitar el acceso a
+                    la ubicación en su navegador.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <FormControlLabel
+                    control={<Checkbox checked={props.noMostrarMas}
+                    onChange={handleCheckboxChange} />}
+                    label="No mostrar más"
+                />
+                <Button variant='contained' color='error'
+                    onClick={handleOkClick} autoFocus>
+                    Ok
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
