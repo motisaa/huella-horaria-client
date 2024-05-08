b9c22f3e8b9063aa09a2e62016bf6038a83e3d40
Moti <motisa94@gmail.com>
2024-05-08 19:20:52 +0200

    feat: change error message text, now only shows error message not error status

--- src/servicios/TratamientoErrores.js
+++ src/servicios/TratamientoErrores.js
@@ -1,15 +1,15 @@
+//esta función maneja los errores que pueden surgir 
+//cuando se hacen solicitudes a través del protocolo HTTP
 export const MensajeError = (err) => {
-  console.log("Tratamiento de errores (error):", err);
   let mens = "";
-  if (err.message) mens += `Error: ${err.message}`;
   if (err.response && err.response.data) {
     if (typeof err.response.data === "string") {
-      mens += ` (${err.response.data})`;
+      mens += `${err.response.data}`;
     } else if (err.response.data.error) {
-      mens += ` (${err.response.data.error})`;
+      mens += ` ${err.response.data.error}`;
     }
     else if (err.response.data.message) {
-      mens += ` (${err.response.data.message})`;
+      mens += ` ${err.response.data.message}`;
     }
   }
   return mens;
