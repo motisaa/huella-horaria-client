ad7450d5cdbbf7a064d62643e74e3bcbc3be7867
Moti <motisa94@gmail.com>
2024-05-25 16:16:36 +0200

    Swap API_URL assignments.

--- src/servicios/Entorno.js
+++ src/servicios/Entorno.js
@@ -1,7 +1,11 @@
 const Entorno = {
     getEnv: () => {
-      //  let API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:8080" : window.API_URL;
-        let API_URL = process.env.NODE_ENV === 'development' ? "https://huellahoraria.ariface2.com" : window.API_URL;
+        let API_URL = process.env.NODE_ENV === 'development'
+            ? "http://localhost:8080"
+            : window.API_URL;
+      /*  let API_URL = process.env.NODE_ENV === 'development' 
+      ? "https://huellahoraria.ariface2.com" : window.API_URL; 
+      */
         API_URL = API_URL || ''
         return {
             API_URL
