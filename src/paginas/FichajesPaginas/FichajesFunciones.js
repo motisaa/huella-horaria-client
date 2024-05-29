40e3db30040065b6a21c6ba20a0b09f412bf7f1f
Moti <109862614+motisaa@users.noreply.github.com>
2024-05-29 10:23:30 +0200

    fix: if the user is not connected to internet, we inform her/him. Geolocalization is possible when it is connected to Internet

--- src/paginas/FichajesPaginas/FichajesFunciones.js
+++ src/paginas/FichajesPaginas/FichajesFunciones.js
@@ -21,12 +21,16 @@ export const validationSchema = () => {
         // con ayuda de ChatGPT
             .test('no-zero', "Para registrar el fichaje, es necesario que el navegador pueda acceder a su ubicación actual."
             + "Por favor, otorgue el permiso correspondiente",
-             value => value !== 0),
+             value => value !== 0)
+            .test('is-not-nan', "Por favor comprueba su conexión, no está conectada/o a internet",
+                 value => !isNaN(value)),
         latitud: yup.number().required("Requerido")
             // con ayuda de ChatGPT
             .test('no-zero', "Para registrar el fichaje, es necesario que el navegador pueda acceder a su ubicación actual.  "
             +"Por favor, otorgue el permiso correspondiente", 
             value => value !== 0)
+            .test('is-not-nan', "Por favor comprueba su conexión, no está conectada/o a internet",
+                value => !isNaN(value)),
       //  trabajadorId:  yup.number().required("Debe elegir un trabajador")
     });
 };
