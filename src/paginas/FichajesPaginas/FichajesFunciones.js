554ef50ef019bc64fdad11c108d8a44ddf611dc8
Moti <motisa94@gmail.com>
2024-05-31 11:44:16 +0200

    feat: add error message for failed geolocation due to not having internet connection

--- src/paginas/FichajesPaginas/FichajesFunciones.js
+++ src/paginas/FichajesPaginas/FichajesFunciones.js
@@ -17,20 +17,22 @@ export const validationSchema = () => {
     // Define validation rules for form fields
     return yup.object({
         tipo: yup.string().required("Requerido"),
-        longitud: yup.number().required("Requerido")
+        longitud: yup.number().required("Obligatorio: Se necesita conexión a internet para localizar su ubicación.")
         // con ayuda de ChatGPT
             .test('no-zero', "Para registrar el fichaje, es necesario que el navegador pueda acceder a su ubicación actual."
             + "Por favor, otorgue el permiso correspondiente",
-             value => value !== 0),
+                value => value !== 0)
+            .transform((value) => (Number.isNaN(value) ? null : value)),
                /* 
             Accept NaN
             .transform((value) => Number.isNaN(value) ? null : value )
             .nullable() */
-        latitud: yup.number().required("Requerido")
+        latitud: yup.number().required("Obligatorio: Se necesita conexión a internet para localizar su ubicación.")
             // con ayuda de ChatGPT
             .test('no-zero', "Para registrar el fichaje, es necesario que el navegador pueda acceder a su ubicación actual.  "
             +"Por favor, otorgue el permiso correspondiente", 
                 value => value !== 0)
+            .transform((value) => (Number.isNaN(value) ? null : value))
             /* 
             Accept NaN
             .transform((value) => Number.isNaN(value) ? null : value )
